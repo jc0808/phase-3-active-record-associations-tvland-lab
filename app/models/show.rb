46117258ffc1a0465f1518e9d615a2adc7c73d2c
Joshua Campos <jdeveloper1203@gmail.com>
2022-10-25 01:18:00 -0400

    commit tree e679366a4615ef2d48af78e363c8ec502e3ce278

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actorsArray = []
+
+    self.actors.each do |actor|
+      actorsArray << "#{actor.first_name} #{actor.last_name}"
+    end
+    actorsArray
+  end
 end
